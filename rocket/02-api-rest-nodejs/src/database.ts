572e287e05506931546b8fb80bc3361c1c99b3c8
Marcus Castro <51335230+OMarcusCastro@users.noreply.github.com>
2023-11-14 20:12:16 -0300

    .

--- rocket/02-api-rest-nodejs/src/database.ts
+++ rocket/02-api-rest-nodejs/src/database.ts
@@ -1,8 +1,11 @@
 import { knex as setupKnex } from 'knex'
 
-export const knex = setupKnex({
+export const config = {
   client: 'sqlite',
   connection: {
-    filename: './temp/app.db',
+    filename: '.temp/app.db',
   },
-})
+  useNullAsDefaut: true,
+}
+
+export const knex = setupKnex(config)
